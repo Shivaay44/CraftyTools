aba7cc35157e838d0630d89ea814d697ce7ff49d
Shivaay <shivaayanime0507@gmail.com>
2026-08-25 21:26:01 +0530

    Update website

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -6,6 +6,23 @@ export const GET: APIRoute = async ({ site, url }) => {
   const tools = getAllTools();
   const currentDate = new Date().toISOString().split('T')[0];
 
+  const staticPages = [
+    { path: '', priority: '1.0', changefreq: 'daily' },
+    { path: '/about', priority: '0.6', changefreq: 'monthly' },
+    { path: '/contact', priority: '0.6', changefreq: 'monthly' },
+    { path: '/privacy', priority: '0.4', changefreq: 'monthly' },
+    { path: '/terms', priority: '0.4', changefreq: 'monthly' },
+  ];
+
+  const staticEntries = staticPages
+    .map((page) => `  <url>
+    <loc>${baseUrl}${page.path}</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>${page.changefreq}</changefreq>
+    <priority>${page.priority}</priority>
+  </url>`)
+    .join('\n');
+
   const toolEntries = tools
     .map((tool) => {
       const priority = '0.8';
@@ -20,12 +37,7 @@ export const GET: APIRoute = async ({ site, url }) => {
 
   const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${baseUrl}/</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>
+${staticEntries}
 ${toolEntries}
 </urlset>`;
 
